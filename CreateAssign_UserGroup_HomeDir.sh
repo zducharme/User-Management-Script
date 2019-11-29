223ee6ef8b4f1a4e628b6e6513b44be6603c210f
zducharme <43576501+zducharme@users.noreply.github.com>
2019-11-29 17:08:34 -0500

    Create CreateAssign_UserGroup_HomeDir.sh

---
+++ CreateAssign_UserGroup_HomeDir.sh
@@ -0,0 +1,17 @@
+#
+## Create home directory README files. (Public and group-based)
+#
+
+sudo touch ~/etc/admins/README.txt
+sudo touch ~/etc/admins/README_PUBLIC.txt
+#
+sudo touch ~/etc/devlopers/README.txt
+sudo touch ~/etc/devlopers/README_PUBLIC.txt
+#
+sudo touch ~/etc/users/README.txt
+sudo touch ~/etc/users/README_PUBLIC.txt
+#
+sudo touch ~/etc/temp/README.txt
+sudo touch ~/etc/temp/README_PUBLIC.txt
+
+# END
